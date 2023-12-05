1dd29ab11e63e7876bc446b7a93a0dc62e6c717f
Venkat4961 <68458929+Venkat4961@users.noreply.github.com>
2023-12-05 21:50:18 +0530

    Add files via upload

---
+++ src/test/java/mavenforjenkins/UITest.java
@@ -0,0 +1,26 @@
+package mavenforjenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class UITest
+{
+    @Test
+    public void startBrowser() {
+       
+
+       
+                WebDriverManager.chromedriver().setup();
+                WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://opensource-demo.orangehrmlive.com/");
+            Assert.assertTrue(driver.getTitle().contains("OrangeHRM"), "Title does not match");
+      
+    }
+}
